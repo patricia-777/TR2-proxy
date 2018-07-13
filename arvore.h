54c42c8f0639bed57632acce74ae7e006df290fd
HelenaSILS <40037397+HelenaSILS@users.noreply.github.com>
2018-07-13 15:17:56 -0300

    Update arvore.h

--- arvore.h
+++ arvore.h
@@ -9,7 +9,7 @@ struct Spider{
 	char modulo[50];	//nome da pasta ou do arquivo entre as barras "/"
 	struct Spider *filho[30];	//lista para armazenar os arquivos ou pastas contidos na pasta pai
 };
-void procuraPath (char *buffer, char *path);
+void procuraPath (char *);
 int pegaPath(char *);
 void pushArvore (char * , int );
 void imprimeSpider();
